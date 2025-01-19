b3a12f06c830e9c8ed4dde7d5f23c6ac157e74b2
pruche <pruche.jm@gmail.com>
2025-01-20 01:21:58 +0900

    ChatServer 파일 추가

---
+++ ChatServerApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+try
+{
+    Console.WriteLine("Start Chat Server!!");
+    
+}
+catch(Exception ex)
+{ 
+    Console.WriteLine(ex.ToString()); 
+}
